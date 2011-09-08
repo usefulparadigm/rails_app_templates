bcb82c7375e2db1daf1e541b3d57cfcde553685c
Sukjoon Kim <sjoonk@gmail.com>
2011-09-08 16:30:59 +0900

    add sample script to backbone

--- backbone.rb
+++ backbone.rb
@@ -12,15 +12,53 @@ inside('public/javascripts') do
   run "mkdir models"
   run "mkdir views"
   run "mkdir routers"
+
   file "app.js", <<-END
+// This is just a sample script. Do not use this as is.
+  
 var App = {
-  Models: {},
-  Views: {},
-  Routers: {},
-  init: function(collection) {
-    Backbone.history.start();
-  }
+	Models: {},
+	Views: {}
 }
+
+App.Router = Backbone.Router.extend({
+	routes: {
+		"": "index"
+	},
+	
+	index: function() {
+		// render the view
+		new App.EntriesIndex();
+	}
+});
+
+App.Models.Entry = Backbone.Model.extend({
+	urlRoot: '/entries'
+});
+
+
+App.Collections.Entries = Backbone.Collection.extend({
+	model: App.Models.Entry,
+	url: '/entries'
+});
+
+
+App.Views.EntriesIndex = Backbone.View.extend({
+	initialize: function() {
+		this.render();
+	},
+	render: function() {
+		$(this.el).html(JST['entries/index']({entries: this.collection}));
+		$('.container').html(this.el);
+		return this;
+	}
+});
+
+$(function() {
+  new App.Router();
+	Backbone.history.start();
+});
+
 END
 end
 
